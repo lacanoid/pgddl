9849f835bd8ace70e280798379a7ad6df1e6455b
Lacanoid <lacanoid@ljudmila.org>
2021-06-16 23:04:15 +0200

    ...

--- ddlx.sql
+++ ddlx.sql
@@ -2824,11 +2824,10 @@ CREATE OR REPLACE FUNCTION ddlx_drop(oid,ddlx_options text[] default '{}')
        when obj.sql_kind = 'SEQUENCE'
        then format(E'DROP %s IF EXISTS %s;\n',obj.sql_kind, obj.sql_identifier)
        when obj.sql_kind is not null
-       then format(E'DROP %s %s%s;%s\n',
+       then format(E'DROP %s %s%s;\n',
                    obj.sql_kind, 
                    case when 'ie' ilike any($2) then 'IF EXISTS ' end,
-                   obj.sql_identifier,
-                   case when obj_kind = 'TABLE' then '-- WARNING!' end
+                   obj.sql_identifier
                    )
        else format(E'-- DROP UNIDENTIFIED OBJECT: %s\n',text($1))
       end
