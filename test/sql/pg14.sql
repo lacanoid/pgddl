89ca51a8f061ad9c32dceadb48c6014e84f5825f
Žiga Kranjec <ziga@ljudmila.org>
2025-02-12 01:58:45 +0100

    tests++

--- test/sql/pg14.sql
+++ test/sql/pg14.sql
@@ -1,7 +1,12 @@
 \i test/sql/pg13.sql
 
+begin;
 -- test compression
 create table complz ( label text );
+create user ddlx_test_user_999;
 alter table complz alter label set compression lz4;
+--grant select on complz to public granted by ddlx_test_user_999;
 
 select ddlx_create('complz'::regclass);
+
+abort;
